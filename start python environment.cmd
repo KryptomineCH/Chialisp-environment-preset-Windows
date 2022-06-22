a4a6b2bf4ceae0d34094f0869d7640af230575bb
Julian B <bechti44@googlemail.com>
2022-06-22 12:36:40 +0200

    updated venv launcher with meaningful title

--- start python environment.cmd
+++ start python environment.cmd
@@ -1,1 +1,6 @@
+@echo off
+:: Set shell Title
+for %%I in (.) do set CurrDirName=%%~nxI
+title %CurrDirName% - venv
+:: move to powershell and start virtual environment
 PowerShell -NoExit "venv\Scripts\activate"
